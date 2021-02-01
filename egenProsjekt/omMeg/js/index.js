295670ba0128730bc62d22831da3abae18b6b84f
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-02-01 09:26:28 +0100

    omMeg update

---
+++ egenProsjekt/omMeg/js/index.js
@@ -0,0 +1,12 @@
+// const checkpoint = 600;
+
+// window.addEventListener("scroll", () => {
+//     const currentScroll = window.pageYOffset;
+//     if(currentScroll <= checkpoint) {
+//         opacity = 0 - currentScroll / checkpoint;
+//     } else {
+//         opacity = 10;
+//     }
+
+//     document.querySelector("#fadeShow").style.opacity = opacity;
+// });
