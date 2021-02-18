496cf0799287e08c61811e1f8aa39e63d2ea197f
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-02-18 08:58:25 +0100

    css / html / view / src

--- fightGame/js/view.js
+++ fightGame/js/view.js
@@ -5,11 +5,10 @@ function view() {
     var menu_div = document.getElementById('Menu')
     //Main Menu
     if(menu) {
-        
         var ul = `<ul>`
         for (var i in menu) {
             if (i == activecursor) {
-                ul += ('<li> >'+ menu[i]+'</li>')
+                ul += ('<li>'+ menu[i]+'</li>')
             }
             else {
                 ul +=('<li>' + menu[i] + '</li>')
