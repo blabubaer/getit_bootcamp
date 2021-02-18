21eafc3a3e834cf78e775980fcbc3b35078fccdb
Jonas Wilhelmsen <jonaswilhelmsen97@gmail.com>
2021-02-18 10:52:06 +0100

    update

--- fightGame/js/view.js
+++ fightGame/js/view.js
@@ -21,40 +21,36 @@ function view() {
         menu_div.innerHTML = text
     }
 
-     // Player-Pokemon
-        //Image
-     var imagePokemonP = document.getElementById('ImagePokemonP')
-     var imgP = `
-     <img src='${player_pokemon.image[1]}' class = "img">`
-     imagePokemonP.innerHTML = imgP
-        //Name
+    // Player-Pokemon
+    //Image
+    var imagePokemonP = document.getElementById('ImagePokemonP')
+    var imgP = `
+    <img src='${player_pokemon.image[1]}' class = "img">`
+    imagePokemonP.innerHTML = imgP
+    //Name
+    // Player Name
     var nametagP = document.getElementById('nametagP')
     nametagP.innerHTML = player_pokemon.name
-        //Healthbar
+    //Healthbar
     var healthbarP = document.getElementById('healtbarP')
     // needs to be updated
     healthbarP.innerHTML = player_pokemon.hp
     
 
     //Enemy-Pokemon
+    // NPC Name tag
+    var nametagE = document.getElementById('nametagE')
+    nametagE.innerHTML = enemy_pokemon.name
+
     var imagePokemonP = document.getElementById('ImagePokemonE')
     var imgP = `
     <img src='${enemy_pokemon.image[0]}'class = "img">`
     imagePokemonP.innerHTML = imgP
-       //Name -> no nametag yet
-   //var nametagP = document.getElementById('nametagP')
-   //nametagP.innerHTML = player_pokemon.name
-       //Healthbar
-   var healthbarP = document.getElementById('healthbarE')
-   // needs to be updated
-   healthbarP.innerHTML = enemy_pokemon.hp
-   var healtP = `
-      <div>
-        <progress max="'+max_hp'">
-            
-        </progress>
-      </div>
-   `
+    
+    //Healthbar
+    var healthbarP = document.getElementById('healthbarE')
+    // needs to be updated
+    healthbarP.innerHTML = enemy_pokemon.hp
 
    
 
